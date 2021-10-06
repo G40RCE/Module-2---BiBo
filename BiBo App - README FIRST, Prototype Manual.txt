29a466a413029e1ecdda21b9c2261d3e6ab4ecc3
G40RCE <jMAvancena@mcm.edu.ph>
2021-10-06 08:43:26 +0800

    User's Manual

---
+++ BiBo App - README FIRST, Prototype Manual.txt
@@ -0,0 +1,30 @@
+Prototype Project Manual
+
+BIBO APP - This is a first ever mobile app, take note there will be non-accurate/real time account system.
+
+Credits who are contributions for this mobile app project:
+James Matthew N. Avancena
+Cariella Jula E. Hilario
+Ria F. Gutierrez
+
+IMPORTANT DISCLAIMER - This is sample mock prototype purposes only.
+We are not responsible for any mistakes, like wrong codes,
+errors, and complex programming. We are take into a considerations to try
+more explore about becoming professional programmers, currently still on the beginners level.
+
+Also, it may depends on the actual size of the mobile application.
+
+Please bear with us....
+
+Here, are the access for the Login Screen:
+BiBo Username: Kevin343
+Password: KevinIsGreat304
+
+Money Transfer details:
+Account ID: 027218010234
+Amount to Send: 200 PHP
+Message: Happy Birthday !
+
+Thank you, and enjoy your pleasure and explore in our app project.
+
+The Dart Source Codes were located in BiBo/biboapp/lib.
--- BiBo/biboapp/lib/main.dart
+++ BiBo/biboapp/lib/main.dart
@@ -4,31 +4,6 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(BiBo());
 }
-// Credits who are contributions for this mobile app project:
-// James Matthew N. Avancena
-// Cariella Jula E. Hilario
-// Ria F. Gutierrez
-
-// BIBO APP - This application is not real-time/accurate.
-// DISCLAIMER - This is sample mock prototype purposes only.
-// We are not responsible for any mistakes, like wrong codes,
-// errors, and complex programming. We are take into a considerations to try
-// more explore about becoming professional programmers, currently still on the beginners level.
-
-// Also, it may depends on the actual size of the mobile application.
-
-// Please bear with us....
-
-// Here, are the manuals for the Login Screen:
-// BiBo Username: Kevin343
-// Password: KevinIsGreat304
-
-// Money Transfer details:
-// Account ID: 027218010234
-// Amount to Send: 200
-// Message: Happy Birthday !
-
-// Thank you, and enjoy your pleasure and explore in our app project.
 
 // for display on the actual splashscreen about the app.
 class BiBo extends StatelessWidget {
